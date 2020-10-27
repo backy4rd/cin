caa1d289d2df4e13037b3dd79cefd9bf572a02d4
backy4rd <khainguyen9209@gmail.com>
2020-10-27 17:43:42 +0700

    create error_handler

--- src/providers/express.ts
+++ src/providers/express.ts
@@ -1,10 +1,15 @@
 import * as express from 'express';
 
+import * as errorHandler from '../utils/error_handler';
+
 const app: express.Application = express();
 
 app.use((req, res, next) => {
   req.local = {};
   next();
 });
+
+app.use(errorHandler.clientErrorHandler);
+app.use(errorHandler.serverErrorHandler);
 
 export default app;
---
+++ src/utils/error_handler.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { AssertionError } from 'chai';
+import logger from '../providers/logger';
+
+export function clientErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AssertionError) {
+    const [status, message] = err.message.split(':');
+    return res.status(parseInt(status)).json({
+      error: { message: message },
+    });
+  }
+
+  next(err);
+}
+
+export function serverErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  logger.error(err);
+
+  return res.status(500).json({
+    error: { message: 'Internal server error' },
+  });
+}

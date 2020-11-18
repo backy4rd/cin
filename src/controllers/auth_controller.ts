1b64c48a4b713f5d5f1b63df625c8e04a1070f3d
backy4rd <khainguyen9209@gmail.com>
2020-11-19 01:42:44 +0700

    init auth route

---
+++ src/controllers/auth_controller.ts
@@ -0,0 +1,63 @@
+import { expect } from 'chai';
+import { Request, Response, NextFunction } from 'express';
+
+import User from '../models/User';
+import asyncHander from '../utils/async_handler';
+
+const jwtRegex = /^[A-Za-z]+ [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/;
+
+class AuthController {
+  @asyncHander
+  public async login(req: Request, res: Response) {
+    const { username, password } = req.body;
+    expect(username && password, '400:Missing parameters').to.exist;
+
+    const user = await User.getUserByUsername(username);
+    expect(user, '404:username not found').to.exist;
+
+    const isMatch = await User.comparePassword(password, user.password);
+    expect(isMatch, "400:Password don't match").to.be.true;
+
+    const jwt = await User.signJWT(user);
+
+    return res.status(200).json({
+      data: { token: jwt },
+    });
+  }
+
+  @asyncHander
+  public async authorize(req: Request, res: Response, next: NextFunction) {
+    const authorization: string = req.headers.authorization;
+
+    expect(authorization, '401:Missing token').to.exist;
+    expect(authorization, '401:Invalid token format').to.match(jwtRegex);
+
+    // prettier-ignore
+    const [/* type */, token] = authorization.split(' ');
+    const jwt = await User.verifyJWT(token);
+
+    req.local.auth = jwt;
+    next();
+  }
+
+  @asyncHander
+  public async changePassword(req: Request, res: Response) {
+    const { old_password, new_password } = req.body;
+    expect(old_password && new_password, '400:Missing parameters').to.exist;
+
+    const username = req.local.auth.username;
+    const user = await User.getUserByUsername(username);
+
+    const isMatch = await User.comparePassword(old_password, user.password);
+    expect(isMatch, "400:Password don't match").to.be.true;
+
+    const hash = await User.hashPassword(new_password);
+    await User.update(username, { password: hash });
+
+    return res.status(200).json({
+      data: { message: 'Change password success' },
+    });
+  }
+}
+
+export default new AuthController();

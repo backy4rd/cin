4df0984065655f8dde793624be16880e407da15f
backy4rd <khainguyen9209@gmail.com>
2020-11-25 18:33:13 +0700

    write validatePostMovieRequest

--- src/controllers/movie_controller.ts
+++ src/controllers/movie_controller.ts
@@ -1,6 +1,8 @@
+import { expect } from 'chai';
 import { Request, Response, NextFunction } from 'express';
 
 import asyncHander from '../decorators/async_handler';
+import { mustExist } from '../decorators/validate_decorators';
 
 class MovieController {
     @asyncHander
@@ -22,7 +24,15 @@ class MovieController {
     public async deleteMovie(req: Request, res: Response) {}
 
     @asyncHander
-    public async validatePostMovieRequest(req: Request, res: Response, next: NextFunction) {}
+    @mustExist('files.movie', 'files.poster', 'body.title')
+    public async validatePostMovieRequest(req: Request, res: Response, next: NextFunction) {
+        const { movie, poster } = req.files;
+
+        expect(movie.type, '400:Invalid filetype').to.match(/video/);
+        expect(poster.type, '400:Invalid filetype').to.match(/image/);
+
+        next();
+    }
 
     @asyncHander
     public async makeHlsFiles(req: Request, res: Response, next: NextFunction) {}

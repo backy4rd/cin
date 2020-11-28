165956867031e7c775d43d2ec07e3130bcfec4af
backy4rd <khainguyen9209@gmail.com>
2020-11-28 22:24:04 +0700

    write createShowtime and updateShowtime

--- src/controllers/showtime_controller.ts
+++ src/controllers/showtime_controller.ts
@@ -4,6 +4,13 @@ import { expect } from 'chai';
 import Showtime from '../models/Showtime';
 import Movie from '../models/Movie';
 
+import {
+    isDateFormat,
+    isNumber,
+    mustExist,
+    mustExistOne,
+    parseDate,
+} from '../decorators/validate_decorators';
 import asyncHander from '../decorators/async_handler';
 
 class ShowtimeController {
@@ -22,7 +29,22 @@ class ShowtimeController {
     }
 
     @asyncHander
-    public async createShowtime(req: Request, res: Response) {}
+    @mustExist('body.movie_id', 'body.start_time')
+    @isNumber('body.movie_id')
+    @isDateFormat('body.start_time')
+    @parseDate('body.start_time')
+    public async createShowtime(req: Request, res: Response) {
+        const { movie_id, start_time } = req.body;
+
+        await Showtime.create({
+            movie_id: movie_id,
+            start_time: start_time,
+        });
+
+        res.status(201).json({
+            data: { message: 'Create success' },
+        });
+    }
 
     @asyncHander
     public async deleteShowtime(req: Request, res: Response) {
@@ -37,7 +59,22 @@ class ShowtimeController {
     }
 
     @asyncHander
-    public async updateShowtime(req: Request, res: Response) {}
+    @mustExistOne('body.start_time')
+    @isDateFormat('body.start_time')
+    @parseDate('body.start_time')
+    public async updateShowtime(req: Request, res: Response) {
+        const showtime_id = parseInt(req.params.showtime_id);
+        const { start_time } = req.body;
+
+        const rowEffect = await Showtime.update(showtime_id, {
+            start_time: start_time,
+        });
+        expect(rowEffect, '404:Showtime not found').to.not.equal(0);
+
+        res.status(200).json({
+            data: { message: 'Update success' },
+        });
+    }
 }
 
 export default new ShowtimeController();
